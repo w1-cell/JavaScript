b951f0b8f1e2d531f5a443c137c0ae10fa9178f9
w1-cell <71689805+w1-cell@users.noreply.github.com>
2020-10-04 21:25:02 +0800

    Add files via upload

---
+++ ball.js
@@ -0,0 +1,43 @@
+//John
+var arr = [89,120,30];
+  var sum = 0;
+  for(var i=0;i<arr.length;i++){
+      sum += arr[i];
+  }
+  var avg1 = sum/arr.length;
+  
+  console.log("John平均分:   " + avg1 );
+  
+  //Mike
+  var arr = [116,94,123];
+  var sum = 0;
+  for(var i=0;i<arr.length;i++){
+      sum += arr[i];
+  }
+  var avg2 = sum/arr.length;
+  console.log("Mike平均分:   " + avg2);
+
+  //Mary
+  var arr = [97,134,105];
+  var sum = 0;
+  for(var i=0;i<arr.length;i++){
+      sum += arr[i];
+  }
+  var avg3 = sum/arr.length;
+  console.log("Mary平均分:   " + avg3);
+  
+  var max;
+  var name;
+  if(avg1 >= avg2){
+  max = avg1 ;
+  name = "胜利者为John，分数为：" ;
+  }else{
+  max = avg2 ;
+  name = "胜利者为Mike，分数为：" ; 
+  }
+  if(max < avg3){
+  max = avg3 ;
+  name = "胜利者为Mary，分数为：" ;
+  }
+  console.log(name + max);
+
---
+++ star.js
@@ -0,0 +1,23 @@
+var str = "";                                        //在控制台输出要采用字符串拼接，所以先定义一个空字符串
+        for (var row = 1; row <= 4; row++) {         //外层循环控制行数,先输出上半部分的4行
+
+            for (var j = 1; j <= 4 - row; j++) {    
+                str = str + " "                      
+            } 
+            for (var i = 1; i <= row; i++) {          //星星数
+                str = str + "* ";
+            }
+            str = str + "\n";                         //外层循环，每输出一行要换行
+        }
+        //下半部分和上半部分大致相同
+        for (var row = 1; row <= 3; row++) {          //外层循环控制行数,输出下半部分的3行
+
+            for (var j = 1; j <= row; j++) {         
+                str = str + " " 
+            } 
+            for (var i = 1; i <= 4 - row; i++) {      //星星数 
+                str = str + "* ";                       
+            }
+            str = str + "\n";                         //外层循环，每输出一行要换行
+        }
+        console.log(str);
